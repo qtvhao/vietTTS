a8de5252bb6dca92ad084c74a0b92320c2fa3faa
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-12-21 06:54:57 +0000

    Reduce LR for every 50k steps.

--- vietTTS/nat/acoustic_trainer.py
+++ vietTTS/nat/acoustic_trainer.py
@@ -63,7 +63,7 @@ val_loss_fn = jax.jit(partial(loss_fn, is_training=False))
 
 loss_vag = jax.value_and_grad(train_loss_fn, has_aux=True)
 
-lr_schedule = optax.exponential_decay(FLAGS.learning_rate, 100_000, 0.5, staircase=True)
+lr_schedule = optax.exponential_decay(FLAGS.learning_rate, 50_000, 0.5, staircase=True)
 
 optimizer = optax.chain(
     optax.clip_by_global_norm(1.0),
