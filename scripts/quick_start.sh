41122960f8acca55d3375523304c686cec706521
Hào Nghiêm <qtvhao@gmail.com>
2022-04-30 23:33:01 +0000

    offload hk_hifi.pickle

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -2,9 +2,9 @@ if [ ! -f assets/infore/hifigan/g_00800000 ]; then
   pip3 install gdown
   echo "Downloading models..."
   mkdir -p -p assets/infore/{nat,hifigan}
-  gdown --id 16UhN8QBxG1YYwUh8smdEeVnKo9qZhvZj -O assets/infore/nat/duration_latest_ckpt.pickle
-  gdown --id 1-8Ig65S3irNHSzcskT37SLgeyuUhjKdj -O assets/infore/nat/acoustic_latest_ckpt.pickle
-  gdown --id 10SFOlAduG20TdjGC5e1Jod_vJIpxkD6u -O assets/infore/hifigan/g_00800000
+  gdown -O assets/infore/nat/duration_latest_ckpt.pickle 16UhN8QBxG1YYwUh8smdEeVnKo9qZhvZj
+  gdown -O assets/infore/nat/acoustic_latest_ckpt.pickle 1-8Ig65S3irNHSzcskT37SLgeyuUhjKdj
+  gdown -O assets/infore/hifigan/g_00800000 10SFOlAduG20TdjGC5e1Jod_vJIpxkD6u
   python -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_00800000
 fi
 
