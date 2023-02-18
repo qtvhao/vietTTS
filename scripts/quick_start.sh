b6e961ae5b1f96eccdc49e1a6a32609ce2eb1a97
willwilson <qtvhao@gmail.com>
2023-02-18 15:18:14 +0700

    fix conflict

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -1,13 +1,12 @@
-if [ ! -f assets/infore/hifigan/g_00800000 ]; then
-  pip3 install gdown
+if [ ! -f assets/infore/hifigan/g_01140000 ]; then
   echo "Downloading models..."
-  mkdir -p -p assets/infore/{nat,hifigan}
-  gdown -O assets/infore/nat/duration_latest_ckpt.pickle 16UhN8QBxG1YYwUh8smdEeVnKo9qZhvZj
-  gdown -O assets/infore/nat/acoustic_latest_ckpt.pickle 1-8Ig65S3irNHSzcskT37SLgeyuUhjKdj
-  gdown -O assets/infore/hifigan/g_00800000 10SFOlAduG20TdjGC5e1Jod_vJIpxkD6u
-  python -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_00800000
+  mkdir -p assets/infore/{nat,hifigan}
+  wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/duration_latest_ckpt.pickle -O assets/infore/nat/duration_latest_ckpt.pickle
+  wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/acoustic_latest_ckpt.pickle -O assets/infore/nat/acoustic_latest_ckpt.pickle
+  wget https://huggingface.co/ntt123/viettts_infore_16k/resolve/main/g_01140000 -O assets/infore/hifigan/g_01140000
+  python3 -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_01140000
 fi
 
 echo "Generate audio clip"
 text=`cat assets/transcript.txt`
-python -m vietTTS.synthesizer --text "$text" --output assets/infore/clip.wav --lexicon-file assets/infore/lexicon.txt --silence-duration 0.2
+python3 -m vietTTS.synthesizer --text "$text" --output assets/infore/clip.wav --lexicon-file assets/infore/lexicon.txt --silence-duration 0.2
