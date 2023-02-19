2ae7611812fffccca0e7e76492ae8394e1450b9a
root <qtvhao@gmail.com>
2023-02-19 06:05:59 -0800

    add

--- linux.sh
+++ linux.sh
@@ -3,3 +3,4 @@ set -e
 docker build -t viettts .
 docker run -it viettts python3 -m vietTTS.synthesizer --text "Xin cho" --output /output/clip0.wav --lexicon-file assets/infore/lexicon.txt --silence-duration 0.2
 docker run -it viettts pytest test_app.py
+docker run -it -p 80:80 --name tts -v "$PWD/output/:/output/" viettts
