a65fb7a2b8447a123d8d94ff6694bde0d4b519ed
root <qtvhao@gmail.com>
2023-02-18 21:08:36 -0800

    add

--- Dockerfile
+++ Dockerfile
@@ -28,17 +28,15 @@ COPY downsample.sh .
 RUN bash downsample.sh
 
 RUN rm downsample.sh
-RUN git clone https://github.com/microsoft/DNS-Challenge && \
-    cd DNS-Challenge/ && git checkout -f 8b87a33b2892f147b5c7ad39ea978453730db269 && \
-    cd NSNet2-baseline/ && \
-    python run_nsnet2.py -i /app/infore_16k/ -o /app/infore_16k_denoised -m ./nsnet2-20ms-baseline.onnx
+#RUN git clone https://github.com/microsoft/DNS-Challenge && \
+#    cd DNS-Challenge/ && git checkout -f 8b87a33b2892f147b5c7ad39ea978453730db269 && \
+#    cd NSNet2-baseline/ && \
+#    python run_nsnet2.py -i /app/infore_16k/ -o /app/infore_16k_denoised -m ./nsnet2-20ms-baseline.onnx
 
-RUN apt-get install -y zip
-COPY package.sh .
-
-RUN bash package.sh
-
-RUN rm package.sh
+#RUN apt-get install -y zip
+#COPY package.sh .
+#RUN bash package.sh
+#RUN rm package.sh
 
 EXPOSE 5000
 
--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, request, jsonify
-import subprocess
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/generate-audio', methods=['POST'])
-def generate_audio():
-    data = request.json
-    text = data.get('text')
-    script_output = subprocess.check_output(['sh', 'path/to/your/script.sh', text])
-    presigned_url = script_output.decode('utf-8').strip()
-    return jsonify({'url': presigned_url})
+@app.route('/synthesizer', methods=['POST'])
+def synthesizer():
+    text = request.form['text']
+    # Run the synthesizer script
+    import subprocess
+    command = f'python -m vietTTS.synthesizer --lexicon-file=train_data/lexicon.txt --text="{text}" --output=clip.wav'
+    subprocess.run(command, shell=True)
+    # Upload the audio file to S3
+    import hashlib
+    hash_value = hashlib.md5(text.encode()).hexdigest()
+    s3_endpoint_url = '<your S3 endpoint URL>'
+    s3_bucket_name = '<your S3 bucket name>'
+    subprocess.run(f'aws s3 cp clip.wav s3://{s3_bucket_name}/{hash_value}.wav --endpoint-url {s3_endpoint_url}', shell=True)
+    # Generate a presigned URL for the audio file
+    presigned_url = subprocess.check_output(f'aws s3 presign s3://{s3_bucket_name}/{hash_value}.wav --endpoint-url {s3_endpoint_url}', shell=True)
+    return f'Presigned URL: {presigned_url}'
 
 if __name__ == '__main__':
     app.run()
